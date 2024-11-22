50b5a5b1755dd6b8dbefa8420a2d92efa93d8274
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2024-11-22 22:43:38 +0100

    Update Tenants

---
+++ src/Tenants.Application/Commands/Tenants/UpfateTenantCommand.cs
@@ -0,0 +1,7 @@
+using MediatR;
+using Tenants.Domain.Entities;
+
+namespace Tenants.Application.Commands
+{
+    public record UpdateTenantCommand(Guid TenantId, string Name, string Description) : IRequest;
+}
